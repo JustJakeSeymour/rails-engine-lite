04564f6a395cedcb89e9cddece708a3ab5420cbb
J Seymour <JustJakeSeymour@gmail.com>
2023-02-06 17:21:04 -0700

    write placeholder tests for section 1 and 2

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "Items API" do
+  describe "Section One RESTful endpoints" do
+    it "sends a list of all items" do
+
+    end
+
+    it "sends a single item"
+
+    it "creates and item"
+
+    it "edits an item"
+
+    it "deletes an item"
+
+    it "merchant information given item ID"
+  end
+
+  describe "Section Two Non-RESTful endpoints" do
+    it "return one item via search OR all items via search"
+
+    it "search via min / max price"
+  end
+end
---
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  describe "Section One RESTFUL endpoints" do
+    it "sends a list of all merchants" do
+      
+    end
+
+    it "sends a single merchant"
+
+    it "sends all items related to a merchant ID"
+  end
+
+  describe "Section Two Non-RESTFUL endpoints" do
+    it "either returns all merchants via search or one via search"
+  end
+end
