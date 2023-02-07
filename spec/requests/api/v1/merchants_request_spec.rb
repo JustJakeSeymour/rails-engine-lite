3310954296df0bae4aeeed160d0595dfbf6231d8
J Seymour <JustJakeSeymour@gmail.com>
2023-02-06 18:02:39 -0700

    merchant show api endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -19,8 +19,20 @@ describe "Merchants API" do
         expect(merchant[:name]).to be_a(String)
       end
     end
-
-    it "sends a single merchant"
+    
+    it "sends a single merchant" do
+      id = create(:merchant).id
+      
+      get "/api/v1/merchants/#{id}"
+      
+      merchant = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).to be_an(Integer)
+  
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).to be_a(String)
+    end
 
     it "sends all items related to a merchant ID"
   end
