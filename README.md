0ab14fddf9c0b6f32252618d58e3fe53e3c2426d
J Seymour <JustJakeSeymour@gmail.com>
2023-02-07 13:30:54 -0700

    post items endpoint

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ This is an API based project, to learn how to parse json from databases using ty
 
 /api/v1...
 [ ] Items
-  [ ] get ...'/items'
-  [ ] get ...'/items/:id'
+  [x] get ...'/items'
+  [x] get ...'/items/:id'
   [ ] post ...'/items'
   [ ] patch ...'/items/:id'
   [ ] destroy ...'/items/:id'
@@ -16,9 +16,9 @@ This is an API based project, to learn how to parse json from databases using ty
     [ ] get ...'/items/find?name=(name)' or 'items/find?min_price=(num)', etc.
 
 [ ] Merchants
-  [ ] get ...'/merchants'
-  [ ] get ...'/merchants/:id'
-  [ ] get ...'/merchants/:id/items'
+  [x] get ...'/merchants'
+  [x] get ...'/merchants/:id'
+  [x] get ...'/merchants/:id/items'
   [ ] search endpoints:
     [ ] get ...'/merchants/find?name=(name)'
 
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -7,11 +7,11 @@ describe "Items API" do
   describe "Section One RESTful endpoints" do
     it "sends a list of all items" do
       create_list(:item, 3, merchant_id: @merchant.id)
-
+      
       get '/api/v1/items'
-
+      
       expect(response).to be_successful
-
+      
       items = JSON.parse(response.body, symbolize_names: true)
       
       expect(items).to have_key(:data)
@@ -40,7 +40,7 @@ describe "Items API" do
         
         expect(item[:attributes]).to have_key(:merchant_id)
         expect(item[:attributes][:merchant_id]).to be_an(Integer)
-
+        
         expect(item[:attributes]).to_not have_key(:created_at)
         expect(item[:attributes]).to_not have_key(:updated_at)
       end
@@ -55,9 +55,9 @@ describe "Items API" do
       
       expect(item).to have_key(:data)
       expect(item[:data]).to be_a(Hash)
-
+      
       item_data = item[:data]
-
+      
       expect(item_data).to have_key(:id)
       expect(item_data[:id]).to be_an(String)
       
@@ -68,29 +68,50 @@ describe "Items API" do
       
       expect(item_data).to have_key(:attributes)
       expect(item_data[:attributes]).to be_a(Hash)
-  
+      
       expect(item_data[:attributes]).to have_key(:name)
       expect(item_data[:attributes][:name]).to be_a(String)
-  
+      
       expect(item_data[:attributes]).to have_key(:description)
       expect(item_data[:attributes][:description]).to be_a(String)
-  
+      
       expect(item_data[:attributes]).to have_key(:unit_price)
       expect(item_data[:attributes][:unit_price]).to be_a(Float)
       
       expect(item_data[:attributes]).to have_key(:merchant_id)
       expect(item_data[:attributes][:merchant_id]).to be_an(Integer)
-
+      
       expect(item_data[:attributes]).to_not have_key(:created_at)
       expect(item_data[:attributes]).to_not have_key(:updated_at)
     end
+    
+    it "creates and item" do
+      create_list(:item, 3, merchant_id: @merchant.id)
+      
+      get '/api/v1/items'
 
-    it "creates and item"
-
+      items = JSON.parse(response.body, symbolize_names: true)
+      
+      expect(items[:data].count).to eq(3)
+      
+      new_json = {
+        "name": "Item",
+        "description": "New Item",
+        "unit_price": 1.99,
+        "merchant_id": @merchant.id
+      }
+      
+      post '/api/v1/items'
+      require 'pry'; binding.pry
+      
+      items = JSON.parse(response.body, symbolize_names: true)
+      expect(items[:data].count).to eq(4)
+    end
+    
     it "edits an item"
-
+    
     it "deletes an item"
-
+    
     it "merchant information given item ID"
   end
 
